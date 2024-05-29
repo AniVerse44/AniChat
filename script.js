07b0034754d97bc423e2cd9aafaa3acb61ad4a34
Ani Verse <69aniverse69@gmail.com>
2024-05-29 01:20:18 -0700

    modified to initial

--- script.js
+++
@@ -1,6 +0,0 @@
-const typeWriter = document.getElementById('typewriter-text');
-const text = '<h4>AniNotes, We provide free Notes Here :D</h4>';
-
-typeWriter.innerHTML = text;
-typeWriter.style.setProperty('--characters', text.length);
-const a = 8;
